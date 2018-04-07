f8bbe312de376bfee3a73751fc0d030cafdf5b4f
Fengxueke <fengxueke@outlook.com>
2018-04-07 13:20:51 +0800

    文件上床页面处理

--- html/rescourses/js/cloud.js
+++ html/rescourses/js/cloud.js
@@ -20,6 +20,9 @@ $(window).ready(function () {
     // $("#user_area_down").css("height", ($(window).height()*0.5) + "px");
     // //$("#user_area_icon").css("height", ($(window).height*0.3)*0.8 + "px");
 
+    //文件名部分长度控制
+    $(".filename-l").css("width",$(".filename-div-l").width() + "px");
+
 });
 
 
@@ -44,24 +47,82 @@ $(window).resize(function () {
     // //设置高度比例
     // $("#user_area_top").css("height", ($(window).height()*0.35) + "px");
     // $("#user_area_down").css("height", ($(window).height()*0.5) + "px");
+
+    //文件名部分长度控制
+    $(".filename-l").css("width",$(".filename-div-l").width() + "px");
 });
 
+//文件上传框
 
-//layer活动窗口控制
-$("#dpage").on("click", function () {
-    layer.open({
-        title: '文件上传', content: $('#myModal'),
-        fixed: true,
-        anim: 2,
-        offset: 'b',
-        shade: 0,
-        type: 1,
-        maxmin: true,
-        move: false,
-        resize: false,
-        area: ['1000px', '200px']
-    }),
-        $("#myModal").removeClass("hidden");
-})
+function toList() {
+    var file = $("#upload");
+    var f_length = file[0].files.length;
+    var items = file[0].files;
+    console.log("文件数量：" + f_length);
+    var fragment = "";
+    if (f_length > 0) {
+        for (var i = 0; i < f_length; i++) {
+            var fileName = items[i].name;
+            var fileSize = items[i].size;
+            var fileType = items[i].type;
+
+
+            var type = /\.[^\.]+$/.exec(fileName);
+
+            var td = $("#upload")[0].files;
+
+            var ico_type = "";
+            //console.log("上传的文件数量" + num);
+
+
+            if(items[i] == $("#upload")[0].files[i]){
+                console.log("上传的xx" + items[i].name);
+                console.log(items[i]);
+            }
+
+            if (type == ".xls" || type == ".xlsx") {
+                ico_type = "fa-file-excel-o"
+            } else if (type == ".docx" || type == ".doc") {
+                ico_type = "fa-file-word-o";
+            } else if (type == ".pptx" || type == ".ppt") {
+                ico_type = "fa-file-powerpoint-o";
+            } else if (type == ".pdf") {
+                ico_type = "fa-file-pdf-o";
+            } else if (type == ".mp3" || type == ".acc") {
+                ico_type = "fa-file-audio-o";
+            } else if (type == ".mp4" || type == ".rmvb" || type == ".mkv" || type == ".avi") {
+                ico_type = "fa-file-video-o";
+            } else if (type == ".txt") {
+                ico_type = "fa-file-text-o"
+            } else if (type == ".jpg" || type == ".dmg" || type == "bmp") {
+                ico_type = "fa-file-image-o";
+            } else {
+                ico_type = "fa-file-o";
+            }
+            fragment += "<tr class='row'><td class='col-md-1' style='text-align: center; vertical-align: middle'><i class='fa " + ico_type + " fa-lg'></i></td><td class='col-md-4'>" + fileName + "</td>"
+                + "<td class='col-md-2'>" + (fileSize / 1024).toFixed(1) + "kb</td> <td class='col-md-2'>/${username}</td><td class='col-md-3'>" + "<div class='progress progress-striped active' style='margin-bottom: 0;'>"+
+                        "<div id='progressBar' class='progress-bar progress-bar-info' role='progressbar' aria-valuemin='0' aria-valuenow='0' aria-valuemax='100' style='width: 0'></div></div></td></tr>";
+            $("#file_list").append(fragment);
+
+            layer.open({
+                title: '文件上传', content: $('#myModal'),
+                fixed: true,
+                anim: 2,
+                offset: 'b',
+                shade: 0,
+                type: 1,
+                maxmin: true,
+                move: false,
+                resize: false,
+                area: ['1000px', '200px']
+            });
+            $("#myModal").removeClass("hidden");
+       }
+        console.log(fragment);
+    } else {
+        console.log("--------error->");
+    }
+}
+
 
 
